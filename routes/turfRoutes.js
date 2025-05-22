5879bb79ce03b5034b555d920423442ae45cec11
Athul N (INFOSYS LIMITED) <v-nathul@microsoft.com>
2025-05-22 14:53:21 +0530

    Implemented turf delete API and access control

--- routes/turfRoutes.js
+++ routes/turfRoutes.js
@@ -8,12 +8,28 @@ router.get('/',async(req,res)=>{
 })
 // Create a turf (only manager or admin)
 router.post('/',auth,async(req,res)=>{
-    if(req.body.role!='admin'&&req.body.role!='manager'){
+    if(req.user.role!='admin'&&req.user.role!='manager'){
         return  res.status(403).json({msg:"Access denied"})
     }
     const turf = new Turf({ ...req.body, managerId: req.user.id });
   await turf.save();
   res.status(201).json(turf);
 });
+router.post('/delete',auth,async (req,res)=>{
+    if(req.user.role!='manager' && req.user.role!='admin'){
+        res.status(403).json({msg:"Access denied for deletion"})
+
+    }
+    try {
+        const deleteTurf= await Turf.findByIdAndDelete(req.body.turfId)
+        if(!deleteTurf)return res.status(404).json({msg:"No turf Found!!"});
+        res.json({ msg: "Turf deleted successfully", deleteTurf });
+    } catch (err) {
+        res.status(500).json({ msg: "Server error", error: err.message });
+        
+    }
+    
+
+})
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,7 +3,7 @@ const auth=require('../middlewares/authMiddleware')
 const express=require('express')
 const router =express.Router()
 router.get('/me',auth,async(req,res)=>{
-    const user=await User.findByID(req.user.id).select('-password')
+    const user=await User.findById(req.user.id).select('-password')
     res.json(user)
 })
 module.exports=router
